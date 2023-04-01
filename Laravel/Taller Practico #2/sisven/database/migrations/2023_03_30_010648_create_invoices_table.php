873c4af2d02ff342720c0fb57c68449cbd8bde47
Jorge Renteria <jorge6565@live.com>
2023-04-01 10:55:13 -0500

    My work

---
+++ Laravel/Taller Practico #2/sisven/database/migrations/2023_03_30_010648_create_invoices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number')->nullable()->unique();
+            $table->date('date')->nullable();
+            $table->foreignId('pay_mode');
+            $table->foreignId('customer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
