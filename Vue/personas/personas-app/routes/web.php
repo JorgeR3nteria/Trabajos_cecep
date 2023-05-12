025cfceef4751bb19c59c584cf42004d70b396f3
Jorge Renteria <jorge6565@live.com>
2023-05-11 23:17:14 -0500

    updated workshop of the Vue practice folder: The api controller is created.

---
+++ Vue/personas/personas-app/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\ComunaController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/comuna', [ComunaController::class, 'index'])->name('comunas.index');
+Route::post('/comuna', [ComunaController::class, 'store'])->name('comunas.store');
+Route::get('/comuna/create', [ComunaController::class,'create'])->name('comunas.create');
+Route::delete('/comuna/{comuna}', [ComunaController::class,'destroy'])->name('comunas.destroy');
+Route::put('/comuna/{comuna}', [ComunaController::class,'update'])->name('comunas.update');
+Route::get('/comuna/{comuna}/edit', [ComunaController::class,'edit'])->name('comunas.edit');
